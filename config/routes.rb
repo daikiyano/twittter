5995eb2d53e6ae4eb459d458eaa531a785d95403
矢野大樹 <>
2018-11-18 13:26:31 +0900

    フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations'}
   get '/users/sign_out' => 'devise/sessions#destroy'
     root "tweeets#index"
-    resources :users
+    resources :users  do
+      member do
+        get :following, :followers
+      end
+    end
   resources :tweeets do
     resources :comments
   end
+  resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
